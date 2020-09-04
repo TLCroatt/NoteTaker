b4827d7d471501eca0ff0a78a4b1e92b1e9a27f7
TLCroatt <trishacroatt@gmail.com>
2020-09-04 14:36:08 -0600

    corrected paths for html and api routes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Dependencies
 var express = require("express");
 var fs = require("fs");
+var path = require("path");
 
 // Sets up the Express App
 var app = express();
@@ -9,34 +10,35 @@ var PORT = 3000;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"))
 
-//Note input data
-var notes = [
-    {
-        routeName: "note",
-        uniqueID: "42"
-    },
-]
 
-//html routes for index and notes
-app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "public/index.html"));
-});
-
+//get requests for html and api
 app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "public/notes.html"));
 });
 
-//create api routes
 app.get("/api/notes", function(req, res) {
-    return res.json(notes);
+    const data = fs.readFileSync("./db/db.json");
+    res.json(JSON.parse(data));
 });
 
+app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+//post & delete requests
 app.post("/api/notes", function(req, res) {
+    //open the db.json file (readFileSync)
+    const data = fs.readFileSync("./db/db.json");
+    //then parse and store in a variable:
+    const parsedData = res.json(JSON.parse(data));
     var newNote = req.body;
-    console.log(newNote);
-    notes.push(newNote);
-    res.json(newNote);
+    //push newNote onto array parsed from db.json file
+    parsedData.push(newNote);
+    //then write to the db.json file (stringify again JSON.stringify(parsedData))
+    fs.writeFileSync("db.json", parsedData);
+    
 });
 
 app.delete("/api/notes/:id", function (req, res) {
